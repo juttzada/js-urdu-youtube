f1885f0bfaf1e0afa1329a6a1387cf712d50f9d3
juttzada <143375761+juttzada@users.noreply.github.com>
2023-10-06 00:57:50 +0000

    data_types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all IS code as newer version
+
+//alert(3 + 3) // we are using nodejs, not browser
+
+let
+name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+
+// number = 2 to power 53
+// bigint
+// string =>
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol = unique
+
+
+//subject
+
+
+
+console. log (typeof undefined); // undefined
+console. log(typeof null); // object
